d82063a838ab48486ab92881f8f29caa3c4d49e7
brickly26 <sabburak26@gmail.com>
2022-11-02 12:09:07 -0400

    created input todo component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 
+//Components
+import InputTodo from './components/InputTodo';
+
 function App() {
   return (
     <>
+    <div>
+      <InputTodo />
+    </div>
     </>
   );
 }
